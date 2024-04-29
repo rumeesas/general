e7083ab76511673bd8e865f0f2a4869df5a047a3
Rumeesa Syed <107001478+rumeesas@users.noreply.github.com>
2024-04-29 18:02:40 -0400

    Update Cholesterol Health Test.py

--- Cholesterol Health Test.py
+++ Cholesterol Health Test.py
@@ -4,7 +4,7 @@ def calculate_ldl(total_chol, hdl_chol):
     return ldl_chol
 
 #function to assess cholesterol level based off of healthy averages for adults
-def classify_cholesterol(ldl_chol):
+def classify_chol(ldl_chol):
     if ldl_chol < 100:
         return "Optimal"
     elif 100 <= ldl_chol < 130:
@@ -22,8 +22,8 @@ hdl_chol = float(input("Input HDL cholesterol level (mg/dL): "))
 
 #calculate user's LDL cholesterol level.
 ldl_chol = calculate_ldl(total_chol, hdl_chol)
-print("Your LDL cholesterol level is:", ldl_chol, "mg/dL")
+print("Your LDL cholesterol level is", ldl_chol, "mg/dL")
 
 #classify whether this is healthy or not for an average adult
-classification = classify_cholesterol(ldl_chol)
-print("This is classified as:", classification,"for an average adult.")
+classification = classify_chol(ldl_chol)
+print("This is classified as", classification,"for an average adult.")
